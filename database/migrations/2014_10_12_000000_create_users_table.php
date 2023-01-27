f40d464c09a71aa59df668b1f80e4195392f8ca1
Sajidul Haque <sajidulhaque007@gmail.com>
2023-01-27 23:00:28 +0600

    user table field modified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('mobile_no')->unique();
-            $table->tinyInteger('otp_verified')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
