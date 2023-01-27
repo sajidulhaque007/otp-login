a0e5294deea22b7b80e1725a67f82dce85dc4746
Sajidul Haque <sajidulhaque007@gmail.com>
2023-01-27 23:01:48 +0600

    user table field modified

--- resources/views/d.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>authenticated</h1>
-<form method="POST" action="{{ route('logout') }}" x-data>
-    @csrf
-
-    <x-jet-responsive-nav-link href="{{ route('logout') }}"
-                   @click.prevent="$root.submit();">
-        {{ __('Log Out') }}
-    </x-jet-responsive-nav-link>
-</form>
